c3438f581bf924d78378931bd2f20649d2bb9655
Peter Breen <pbreen@gmail.com>
2016-03-18 11:35:23 -0700

    add sumArray() function

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var sum = function(sumNumber1, sumNumber2) {
 //problem 2 - multiply function
 var multiply = function(multiplyNumber1, multiplyNumber2) {
   multiplyTotal = multiplyNumber1 * multiplyNumber2;
-  return multiplyTotal; 
+  return multiplyTotal;
   //console.log('The product of ' + multiplyNumber1 + ' and ' + multiplyNumber2 + ' is ' + multiplyTotal + '.');
 };
 
@@ -19,4 +19,14 @@ var sumAndMultiply = function(samNumber1, samNumber2, samNumber3) {
   console.log(samNumber1 + ' and ' + samNumber2 + ' and ' + samNumber3 + ' sum to ' + samSum + '.');
   console.log('The numbers ' + samNumber1 + ' and ' + samNumber2 + ' and ' + samNumber3 + ' have a product of ' + samProduct + '.');
   return [samSum, samProduct];
+};
+
+//problem 4 - sum of an array
+var sumArray = function(inputSumArray) {
+  var outputSumArray = 0;
+  console.table(inputSumArray);
+  for (var i = 0; i < inputSumArray.length; i++) {
+    outputSumArray += inputSumArray[i];
+  }
+  console.log(inputSumArray + ' was passed in as an array of numbers and ' + outputSumArray + ' is their sum.');
 };
